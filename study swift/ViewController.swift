06eedcda06cf31d3c6f657af7bdb770529923906
kashinotti <ryo.ka.notti@gmail.com>
2021-02-22 22:32:32 +0900

    Initial Commit

---
+++ study swift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  study swift
+//
+//  Created by 大野　竜太 on 2021/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("hello world")
+    }
+
+
+}
+
