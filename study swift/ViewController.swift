fb6615256132e7615d9b1ea17242d90a43e3953b
kashinotti <ryo.ka.notti@gmail.com>
2021-02-23 10:52:53 +0900

    変数・定数の学習

--- study swift/ViewController.swift
+++ study swift/ViewController.swift
@@ -14,6 +14,20 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         print("hello world")
         print("hello github")
+         
+//        変数の定義と値の代入
+        var a: Int
+        a = 1
+        
+//        変数・定数の宣言と同時に値を代入できる
+        let b: Int = 123
+        
+//        代入する値から型アノテーションを推測して省略することができる
+        let c = 456
+        let d = "abc"
+        
+        let e = 111
+        type(of: e)
     }
 
 
